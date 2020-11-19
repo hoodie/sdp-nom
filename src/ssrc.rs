c0ffee27070df02d866764d5d9d3fd34f2b4a023
Hendrik Sollich <hendrik@hoodie.de>
2020-11-19 18:04:08 +0100

    refactor: add test for not yet supported ssrc lines

--- src/ssrc.rs
+++ src/ssrc.rs
@@ -52,3 +52,20 @@ fn test_ssrc_line() {
         Ssrc { id: 1366781084, attribute: "cname", value: "EocUG1f0fcg/yvY7" }
     );
 }
+#[test]
+#[rustfmt::skip]
+#[ignore]
+fn test_unsupported_ssrc_line() {
+    assert_line!(ssrc_line, "a=ssrc:3570614608 cname:4TOk42mSjXCkVIa6");
+    assert_line!(ssrc_line, "a=ssrc:3570614608 msid:lgsCFqt9kN2fVKw5wg3NKqGdATQoltEwOdMS 35429d94-5637-4686-9ecd-7d0622261ce8");
+    assert_line!(ssrc_line, "a=ssrc:3570614608 mslabel:lgsCFqt9kN2fVKw5wg3NKqGdATQoltEwOdMS");
+    assert_line!(ssrc_line, "a=ssrc:3570614608 label:35429d94-5637-4686-9ecd-7d0622261ce8");
+    assert_line!(ssrc_line, "a=ssrc-group:FID 2231627014 632943048");
+    assert_line!(ssrc_line, "a=ssrc:2231627014 cname:4TOk42mSjXCkVIa6");
+    assert_line!(ssrc_line, "a=ssrc:2231627014 msid:lgsCFqt9kN2fVKw5wg3NKqGdATQoltEwOdMS daed9400-d0dd-4db3-b949-422499e96e2d");
+    assert_line!(ssrc_line, "a=ssrc:2231627014 mslabel:lgsCFqt9kN2fVKw5wg3NKqGdATQoltEwOdMS");
+    assert_line!(ssrc_line, "a=ssrc:2231627014 label:daed9400-d0dd-4db3-b949-422499e96e2d");
+    assert_line!(ssrc_line, "a=ssrc:632943048 cname:4TOk42mSjXCkVIa6");
+    assert_line!(ssrc_line, "a=ssrc:632943048 msid:lgsCFqt9kN2fVKw5wg3NKqGdATQoltEwOdMS daed9400-d0dd-4db3-b949-422499e96e2d");
+}
+
